fe89972ce11d78f7f8c6385602054ea6f9dd23e0
elizabeth.84 <elizabeth.azanero@gmail.com>
2016-08-25 13:23:06 -0700

    subiendo el segune ejercicio

---
+++ app.js
@@ -0,0 +1,5 @@
+var num1= parseFloat(prompt("Ingrese la primera nota"));
+var num2= parseFloat(prompt("Ingrese la segunda nota"));
+var num3= parseFloat(prompt("Ingrese la tercera nota"));
+promedio=(num1+num2+num3)/3;
+document.write("Su promedio es :" + promedio);
---
+++ readme.md
@@ -0,0 +1,15 @@
+###Ejercicio2
+
+##### Pseudocodigo
+
+1. Inicio
+2. Ingrese num1
+3. Ingrese num2
+4. Ingrese num3
+5. Calcular el promedio=(num1+num2+num3)/3
+6. Resultado promedio
+7. Fin
+
+#### Imagen
+
+![imagen](http://i65.tinypic.com/1z65wdu.jpg)
